98cd72eb73f118a572e0ebef51aa7cc91c694372
Tom Belton <tombelton@hotmail.co.uk>
2024-05-10 17:52:56 +0200

    added QueryClientProvider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,10 @@
 import './App.css';
+import React from 'react';
+import { AppContainer } from './components/AppContainer';
+import { QueryClientProvider } from './context/react-query';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://www.searchpilot.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn more about SearchPilot
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => (
+  <QueryClientProvider>
+    <AppContainer />
+  </QueryClientProvider>
+);
