5820ccbe36b49b9de3e731a05c6004c129e57290
Tom Belton <tombelton@hotmail.co.uk>
2024-05-10 17:01:17 +0200

    initial commit - added base repo code

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.searchpilot.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn more about SearchPilot
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
